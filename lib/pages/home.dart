cdc0842b5a7b3028e008f639b08e9260884e61d3
Shiva <146922651+JosephVJFC@users.noreply.github.com>
2023-11-17 11:48:52 +0530

    17-11-23 home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider_frontend/model/user_detailprovider.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_frontend/pages/gardening.dart';
 import '../model/user_detail_model.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
@@ -56,6 +57,11 @@ class _HomeState extends State<Home> {
     });
   }
 
+  void _navigateToNextScreen(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => GardeningFunction()));
+  }
+
   @override
   void initState() {
     getuserDetails();
@@ -93,8 +99,7 @@ class _HomeState extends State<Home> {
                               color: Colors.grey,
                             ),
                           ),
-                          Text(
-                            userdetails.name,
+                          Text(userdetails.name,
                               style: GoogleFonts.commissioner(
                                 fontSize: 19,
                                 fontWeight: FontWeight.w700,
@@ -105,37 +110,22 @@ class _HomeState extends State<Home> {
 
                       // Notification//
                       Container(
-                        decoration: BoxDecoration(
-                            // color: Colors.red,
-                            border: Border.all(
-                                color:
-                                    const Color.fromARGB(255, 196, 195, 195)),
-                            borderRadius: BorderRadius.circular(50)),
-                        child:IconButton(
-                          icon:const Icon(
+                          decoration: BoxDecoration(
+                              // color: Colors.red,
+                              border: Border.all(
+                                  color:
+                                      const Color.fromARGB(255, 196, 195, 195)),
+                              borderRadius: BorderRadius.circular(50)),
+                          child: IconButton(
+                            icon: const Icon(
                               Icons.person,
                               color: Color.fromRGBO(157, 118, 193, 1),
-
                             ),
-                          iconSize: 30,
-                          onPressed: (){
-                            Navigator.pushNamed(context, '/profile');
-
-                          },
-                        )
-
-
-                        // padding: EdgeInsets.all(12),
-                        //
-                        // child: const Icon(
-                        //   Icons.person,
-                        //   color: Color.fromRGBO(157, 118, 193, 1),
-                        //   size: 26,
-                        //
-                        // ),
-
-
-                      ),
+                            iconSize: 30,
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/profile');
+                            },
+                          )),
                     ],
                   ),
 
@@ -202,229 +192,75 @@ class _HomeState extends State<Home> {
                     ],
                   ),
 
-                  const SizedBox(height: 35),
+                  const SizedBox(height: 55),
 
                   Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          // for (var i = 1; i < 4; i++)
-                          // SizedBox(height: 1),
-                          Column(
-                            children: [
-                              // Row 1//
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Container(
-                                  child:Image.asset("assets/images/provider/gardening.png",
-                                  height: 10,
-                                  ),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
+                      child: GestureDetector(
+                    onTap: () {
+                      _navigateToNextScreen(context);
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(left: 30, right: 30),
+                      child: GridView.count(
+                        physics: const BouncingScrollPhysics(),
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 8.0,
+                        mainAxisSpacing: 8.0,
+                        childAspectRatio: 3 / 3,
+                        shrinkWrap: true,
+                        children: List.generate(
+                          6,
+                          (index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Container(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: 50),
+                                  child: Container(
+                                      child: Column(
+                                    children: [
+                                      Image.asset(
+                                        "lib/assets/provider/car.png",
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(top: 10),
+                                        child: Text(
+                                          'Driving',
+                                          style: GoogleFonts.commissioner(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w500,
+                                              color: Colors.white),
                                         ),
-                                      ],
-                                    ),
+                                      )
+                                    ],
+                                  )),
+                                ),
+                                decoration: const BoxDecoration(
+                                  color: Color.fromRGBO(157, 118, 193, 1),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20.0),
                                   ),
-                                  Container(
-                                   child:Image.asset("assets/images/provider/healthcare_3825386 (1) 2.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
+                                ),
                               ),
-
-                              // End Row 1//
-
-                              const SizedBox(
-                                height: 20,
-                              ),
-
-                              // Row 2 //
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Container(
-                                    child:Image.asset("assets/images/provider/cooking_2975608 2.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                    child:Image.asset("assets/images/provider/steering-wheel_7648986 2.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                              // End Row 2//
-                              const SizedBox(
-                                height: 20,
-                              ),
-
-                              // Row 3 //
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Container(
-                                   child:Image.asset("assets/images/provider/bag_2430422 2.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                    child:Image.asset("assets/images/provider/broom_716996 2.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-
-                              // End Row 3//
-                              const SizedBox(
-                                height: 20,
-                              ),
-
-                              // Row 4 //
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Container(
-                                    child:Image.asset("assets/images/provider/man_7755397 2.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                   child: Image.asset("assets/images/provider/Group 37067.png",),
-                                    width: 80,
-                                    height: 80,
-                                    decoration: const BoxDecoration(
-                                      color: Colors.white,
-                                      // borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Color.fromRGBO(
-                                              157, 118, 193, 0.3),
-                                          blurRadius: 0.2,
-                                          offset:
-                                              Offset(1, 2), // Shadow position
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              // End Row 4//
-                            ],
-                          ),
-                        ],
+                            );
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                  // SizedBox(height: 10,),
+                  )),
                 ],
               ),
             ),
             bottomNavigationBar: CurvedNavigationBar(
-                color: Color.fromRGBO(157, 118, 193, 1),
+                color: const Color.fromRGBO(157, 118, 193, 1),
                 height: 48,
                 backgroundColor: Colors.white,
                 items: const [
-                  Icon(Icons.add,),
+                  Icon(
+                    Icons.add,
+                  ),
                 ]),
           )));
-    }
-    );
+    });
   }
 }
